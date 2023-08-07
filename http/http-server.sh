f873ee565fd6b064f9f4231068ea5fc631f2d84f
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-08-07 12:05:21 +0900

    Update http-server.sh

--- http/http-server.sh
+++ http/http-server.sh
@@ -3,5 +3,5 @@ PORT=8080
 echo "Listening on port" ${PORT}
 while true
 do
-	( echo "HTTP/1.0 200 OZ"; echo; echo -n "OK") | nc -l -p ${PORT} -w 1
+	( echo "HTTP/1.0 200 Ok"; echo; echo -n "OK") | nc -l -p ${PORT} -w 1
 done
