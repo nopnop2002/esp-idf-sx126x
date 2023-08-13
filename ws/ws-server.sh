0a01d50e1ae3c15285c51ab3888ef3ac28a84ede
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-08-13 14:18:42 +0900

    delete ws

--- ws/ws-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Install websocat
-#sudo wget -qO /usr/local/bin/websocat https://github.com/vi/websocat/releases/latest/download/websocat.x86_64-unknown-linux-musl
-#sudo chmod a+x /usr/local/bin/websocat
-#websocat --version
-PORT=8080
-websocat -E -t ws-listen:0.0.0.0:${PORT} literalreply:'OK'
