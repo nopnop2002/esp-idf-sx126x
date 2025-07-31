b2146083d080396dcb9b437672dcf3b1db7e0116
nopnop2002 <>
2025-07-31 15:03:20 +0900

    added interrupt project

---
+++ interrupt/README.md
@@ -0,0 +1,52 @@
+# Interrupt Example   
+The SX126x supports the following interrupts:   
+This project demonstrates how to use TxDone or RxDone.   
+
+|Bit|IRQ|Description|Modulation|
+|:-:|:-:|:-:|:-:|
+|0|TxDone|Packet transmission completed|All|
+|1|RxDone|Packet received|All|
+|2|PreambleDetected|Preamble detected|All|
+|3|SyncWordValid|Valid sync word detected|FSK|
+|4|HeaderValid|Valid LoRa header received|LoRa|
+|5|HeaderErr|LoRa header CRC error|LoRa|
+|6|CrcErr|Wrong CRC received|All|
+|7|CadDone|Channel activity detection finished|LoRa|
+|8|CadDetected|Channel activity detected|LoRa|
+|9|Timeout|Rx or Tx timeout|All|
+
+
+# Configuration   
+![Image](https://github.com/user-attachments/assets/381a395e-e04f-4fdd-9579-f8353d44245b)
+![Image](https://github.com/user-attachments/assets/292df250-48d8-489c-9beb-1ecaba7fc37c)
+
+# Wirering
+Add a wire to DIO1.   
+|SX126X||ESP32|ESP32-S2/S3|ESP32-C2/C3/C6|
+|:-:|:-:|:-:|:-:|:-:|
+|BUSY|--|GPIO17|GPIO39|GPIO3|
+|RST|--|GPIO16|GPIO38|GPIO2|
+|TXEN|--|N/C|N/C|N/C|
+|RXEN|--|N/C|N/C|N/C|
+|MISO|--|GPIO19|GPIO37|GPIO4|
+|SCK|--|GPIO18|GPIO36|GPIO5|
+|MOSI|--|GPIO23|GPIO35|GPIO6|
+|NSS|--|GPIO15|GPIO34|GPIO7|
+|GND|--|GND|GND|GND|
+|VCC|--|3.3V|3.3V|3.3V|
+|DIO1|--|GPIO21|GPIO1|GPIO1|
+
+# How to use interrupt
+irqMask sets the interrupt bits you need. You can set multiple bits.   
+dioXMask sets the interrupt bits to notify DIOX. You can set multiple bits.   
+The following setting enables all interrupts:   
+When SX126X_IRQ_TX_DONE occurs, DIO1 turns ON.   
+When SX126X_IRQ_RX_DONE occurs, DIO2 turns ON.
+When SX126X_IRQ_TIMEOUT occurs, DIO3 turns ON.
+```
+    uint16_t irqMask = SX126X_IRQ_ALL;
+    uint16_t dio1Mask = SX126X_IRQ_TX_DONE;
+    uint16_t dio2Mask = SX126X_IRQ_RX_DONE;;
+    uint16_t dio3Mask = SX126X_IRQ_TIMEOUT;
+    SetDioIrqParams(irqMask, dio1Mask, dio2Mask, dio3Mask);
+```
---
+++ interrupt/main/main.c
@@ -0,0 +1,202 @@
+/* The example of ESP-IDF
+ *
+ * This sample code is in the public domain.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#include "ra01s.h"
+
+#define GPIO_INPUT_IO CONFIG_INPUT_GPIO
+#define GPIO_INPUT_PIN_SEL	(1ULL<<GPIO_INPUT_IO)
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static const char *TAG = "MAIN";
+
+static QueueHandle_t gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+	uint32_t gpio_num = (uint32_t) arg;
+	xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+void gpio(void *pvParameters)
+{
+	uint32_t io_num;
+	while (1) {
+		if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+			ESP_LOGW(pcTaskGetName(NULL), "GPIO[%"PRIu32"] intr, val: %d", io_num, gpio_get_level(io_num));
+		} else {
+			ESP_LOGE(pcTaskGetName(NULL), "xQueueReceive fail");
+			break;
+		}
+	}
+}
+
+#if CONFIG_SENDER
+void task_tx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+
+	// Set up an interrupt for SPI transactions
+	uint16_t irqMask = SX126X_IRQ_TX_DONE;
+	uint16_t dio1Mask = SX126X_IRQ_TX_DONE;
+	uint16_t dio2Mask = 0;
+	uint16_t dio3Mask = 0;
+	SetDioIrqParams(irqMask, dio1Mask, dio2Mask, dio3Mask);
+
+	uint8_t buf[255]; // Maximum Payload size of SX1261/62/68 is 255
+	while(1) {
+		TickType_t nowTick = xTaskGetTickCount();
+		int txLen = sprintf((char *)buf, "Hello World %"PRIu32, nowTick);
+		ESP_LOGI(pcTaskGetName(NULL), "%d byte packet sent...", txLen);
+
+		// Wait for transmission to complete
+		if (LoRaSend(buf, txLen, SX126x_TXMODE_SYNC) == false) {
+			ESP_LOGE(pcTaskGetName(NULL),"LoRaSend fail");
+		}
+
+		// Do not wait for the transmission to be completed
+		//LoRaSend(buf, txLen, SX126x_TXMODE_ASYNC );
+
+		int lost = GetPacketLost();
+		if (lost != 0) {
+			ESP_LOGW(pcTaskGetName(NULL), "%d packets lost", lost);
+		}
+
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	} // end while
+
+	// never reach here
+	vTaskDelete( NULL );
+}
+#endif // CONFIG_SENDER
+
+
+
+#if CONFIG_RECEIVER
+void task_rx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+
+	// Set up an interrupt for SPI transactions
+	uint16_t irqMask = SX126X_IRQ_RX_DONE;
+	uint16_t dio1Mask = SX126X_IRQ_RX_DONE;
+	uint16_t dio2Mask = 0;
+	uint16_t dio3Mask = 0;
+	SetDioIrqParams(irqMask, dio1Mask, dio2Mask, dio3Mask);
+
+	uint8_t buf[255]; // Maximum Payload size of SX1261/62/68 is 255
+	while(1) {
+		//SetDioIrqParams(irqMask, dio1Mask, dio2Mask, dio3Mask);
+		uint8_t rxLen = LoRaReceive(buf, sizeof(buf));
+		if ( rxLen > 0 ) { 
+			ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s]", rxLen, rxLen, buf);
+
+			int8_t rssi, snr;
+			GetPacketStatus(&rssi, &snr);
+			ESP_LOGI(pcTaskGetName(NULL), "rssi=%d[dBm] snr=%d[dB]", rssi, snr);
+		}
+		vTaskDelay(1); // Avoid WatchDog alerts
+	} // end while
+
+	// never reach here
+	vTaskDelete( NULL );
+}
+#endif // CONFIG_RECEIVER
+
+void app_main()
+{
+	//zero-initialize the config structure.
+	gpio_config_t io_conf = {};
+	//interrupt of rising edge
+	io_conf.intr_type = GPIO_INTR_POSEDGE;
+	//set as input mode
+	io_conf.mode = GPIO_MODE_INPUT;
+	//bit mask of the pins that you want to set
+	io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+	//disable pull-down mode
+	io_conf.pull_down_en = 0;
+	//disable pull-up mode
+	io_conf.pull_up_en = 0;
+	gpio_config(&io_conf);
+	//configure GPIO with the given settings
+	gpio_config(&io_conf);
+
+	//create a queue to handle gpio event from isr
+	gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+
+	//install gpio isr service
+	gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+	//hook isr handler for specific gpio pin
+	gpio_isr_handler_add(GPIO_INPUT_IO, gpio_isr_handler, (void*) GPIO_INPUT_IO);
+
+	// Initialize LoRa
+	LoRaInit();
+	int8_t txPowerInDbm = 22;
+
+	uint32_t frequencyInHz = 0;
+#if CONFIG_433MHZ
+	frequencyInHz = 433000000;
+	ESP_LOGI(TAG, "Frequency is 433MHz");
+#elif CONFIG_866MHZ
+	frequencyInHz = 866000000;
+	ESP_LOGI(TAG, "Frequency is 866MHz");
+#elif CONFIG_915MHZ
+	frequencyInHz = 915000000;
+	ESP_LOGI(TAG, "Frequency is 915MHz");
+#elif CONFIG_OTHER
+	ESP_LOGI(TAG, "Frequency is %dMHz", CONFIG_OTHER_FREQUENCY);
+	frequencyInHz = CONFIG_OTHER_FREQUENCY * 1000000;
+#endif
+
+#if CONFIG_USE_TCXO
+	ESP_LOGW(TAG, "Enable TCXO");
+	float tcxoVoltage = 3.3; // use TCXO
+	bool useRegulatorLDO = true; // use DCDC + LDO
+#else
+	ESP_LOGW(TAG, "Disable TCXO");
+	float tcxoVoltage = 0.0; // don't use TCXO
+	bool useRegulatorLDO = false; // use only LDO in all modes
+#endif
+
+	//LoRaDebugPrint(true);
+	if (LoRaBegin(frequencyInHz, txPowerInDbm, tcxoVoltage, useRegulatorLDO) != 0) {
+		ESP_LOGE(TAG, "Does not recognize the module");
+		while(1) {
+			vTaskDelay(1);
+		}
+	}
+	
+	uint8_t spreadingFactor = 7;
+	uint8_t bandwidth = 4;
+	uint8_t codingRate = 1;
+	uint16_t preambleLength = 8;
+	uint8_t payloadLen = 0;
+	bool crcOn = true;
+	bool invertIrq = false;
+#if CONFIG_ADVANCED
+	spreadingFactor = CONFIG_SF_RATE;
+	bandwidth = CONFIG_BANDWIDTH;
+	codingRate = CONFIG_CODING_RATE;
+#endif
+	LoRaConfig(spreadingFactor, bandwidth, codingRate, preambleLength, payloadLen, crcOn, invertIrq);
+
+	xTaskCreate(&gpio, "GPIO", 1024*2, NULL, 5, NULL);
+#if CONFIG_SENDER
+	xTaskCreate(&task_tx, "TX", 1024*4, NULL, 5, NULL);
+#endif
+#if CONFIG_RECEIVER
+	xTaskCreate(&task_rx, "RX", 1024*4, NULL, 5, NULL);
+#endif
+}
+
